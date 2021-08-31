f4297acb31316a902be476920b6afa65e4deb0db
Gabe Mulley <gnm112117@gmail.com>
2021-08-31 06:34:12 -0400

    update

--- example/src/App.js
+++ example/src/App.js
@@ -20,19 +20,31 @@ const App = () => {
   return (
     <ExperimentSwitch
       experimentId="exp1"
-      onError={console.log}
-      loadingIndicator={<div>Loading...</div>}
       render={
-        ({group, variables, reasonCode}) => {
-          return (<>
-            <div>Reason Code: {reasonCode}</div>
-            <div>Assignment: {group}</div>
-            {variables && variables.image_url && <img src={variables.image_url} alt="foo" width="300" />}
-            <p><a id="convert-link" href="https://www.google.com" onClick={handleClick}>Pay!</a></p>
-            {variables && variables.styles && <RuntimeStylesheet
-              content={variables.styles}
-            />}
-          </>)
+        ({assignment, isLoading, isEligible, error}) => {
+          if (isLoading) {
+            return (<div>Loading...</div>);
+          }
+          if (!isEligible) {
+            return (<div>Not eligible</div>);
+          }
+          if (error != null) {
+            console.log(error);
+            return (<div>Error occurred!</div>);
+          }
+          if (assignment !== null) {
+            const {group, reasonCode, variables} = assignment;
+            return (<>
+              <div>Reason Code: {reasonCode}</div>
+              <div>Assignment: {group}</div>
+              {variables && variables.image_url && <img src={variables.image_url} alt="foo" width="300" />}
+              <p><a id="convert-link" href="https://www.google.com" onClick={handleClick}>Pay!</a></p>
+              {variables && variables.styles && <RuntimeStylesheet
+                content={variables.styles}
+              />}
+            </>);
+          }
+          return null;
         }
       }
     />
--- src/index.js
+++ src/index.js
@@ -1,66 +1,65 @@
-import React from 'react'
+import React from 'react';
 import hypo from 'hypo-client'
 
 class ExperimentSwitch extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isLoaded: false,
-      group: null
+      isEligible: true,
+      assignment: null,
+      error: null,
     };
   }
 
   componentDidMount() {
+    if (typeof this.props.isEligible === "function" && this.props.isEligible() === false) {
+      this.setState({isEligible: false, isLoaded: true});
+      return;
+    }
     hypo.getGroupAssignment(this.props.experimentId).then(
       (grp) => {
-        this.setState({group: grp, isLoaded: true})
+        this.setState({assignment: grp, isLoaded: true});
       }, (err) => {
-        if (this.props.onError) {
-          this.props.onError(err)
-        }
+        this.setState({error: err, isLoaded: true});
       }
     )
   }
 
   render() {
-    if (this.state.isLoaded) {
-      return this.props.render(this.state.group)
-    } else if(this.props.loadingIndicator) {
-      return this.props.loadingIndicator
-    } else {
-      return;
-    }
+    return this.props.render(this.state);
   }
 }
 
+
 class RuntimeStylesheet extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
-    const {href, content, ...innerProps} = this.props
+    const {href, content, ...innerProps} = this.props;
     if (href) {
       return (
         <link rel="stylesheet" type="text/css" href={href} {...innerProps} />
-      )
+      );
     } else {
       return (<style type="text/css" {...innerProps}>
         {content}
-      </style>)
+      </style>);
     }
   }
 }
 
 class RuntimeHtml extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
     return (
       <div dangerouslySetInnerHTML={{'__html': this.props.content}} />
-    )
+    );
   }
 }
 
